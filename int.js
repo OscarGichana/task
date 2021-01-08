9006d14e2de85a9ee297a278190b05dab2739f5d
Oscar Gichana <gichanaoscar3@gmail.com>
2021-01-08 09:50:45 +0300

    tasks

---
+++ int.js
@@ -0,0 +1,26 @@
+
+s
+
+
+
+function integer(array, start, end){
+  if (start > end) 
+    return end + 1; 
+
+  if (start != array[start]) 
+    return start; 
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ sen.js
@@ -0,0 +1,26 @@
+var sen = getElementById("fname");
+var dl = 20;
+if(sen.length == dl){
+  console.log("sen");
+}else if(sen.length > dl){
+  let short_sentence = sentence.slice(0, d_length)
+  console.log(short_sentence)
+};
+
+
+<script> 
+
+var numbers = [prompt("Please enter your list of numbers", "")];
+numbers.forEach(myFunction)
+
+function myFunction(){
+  if (numbers == []) {
+    confirm("sentence")
+  }else if(numbers!== []){
+    confirm("short_sentence")
+  }else{
+    confirm(hihi)
+  }
+}
+
+</script>
